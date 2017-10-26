5d4f257aeff2e7389f273f29724cab6aa8e8a33b
darkain <git@darkain.com>
2017-10-26 01:34:05 -0700

    Added "heart" animation

--- anim.cpp
+++ anim.cpp
@@ -12,32 +12,58 @@ void anim::frame(pixelArray **strip, WII **wii) {
 		if (!wii[i]->wiimoteConnected) continue;
 
 		if (wii[i]->getButtonClick(ONE)) {
+			strip[0]->clear();
 			reset(0, GRID_WIDTH);
 		}
 
 
 		if (wii[i]->getButtonClick(TWO)) {
+			strip[0]->clear();
 			reset(1);
 		}
 	}
 
 
-	strip[0]->clear();
 
 
 	switch (mode) {
 		case 0:
-			const char *text = "KAEATRI";
-
 			if (elapsed >= SCROLL_DELAY) {
 				elapsed -= SCROLL_DELAY;
 				cycle--;
-				if (cycle < 0-pixelArray::stringWidth(text)) {
+				if (cycle < 0-pixelArray::stringWidth("KAEATRI")) {
 					cycle = (strip[0]->width()) + 5;
 				}
 			}
 
-			strip[0]->string(text, cycle, strip[0]->height()-5);
+			strip[0]->clear();
+			strip[0]->string("KAEATRI", cycle, strip[0]->height()-5);
+		break;
+
+
+
+
+		////////////////////////////////////////////////////////////////////////
+		// Custom "heart" animation for YOU KNOW WHO ;)
+		////////////////////////////////////////////////////////////////////////
+		case 1:
+			pixel_command current = command_list[cycle];
+			while (elapsed >= current.delay) {
+				elapsed -= current.delay;
+
+				if (current.x == 255  &&  current.y == 255) {
+					strip[0]->clear();
+				} else {
+					strip[0]->draw(current.x, current.y, color_t::red());
+				}
+
+				cycle++;
+				if (cycle >= (sizeof(command_list)/sizeof(pixel_command))) {
+					cycle = 0;
+				}
+
+				current = command_list[cycle];
+			}
 		break;
 	}
 
