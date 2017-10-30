ebbf66903df39335c03d25c05c52a959f68059cc
darkain <git@darkain.com>
2017-10-29 23:30:07 -0700

    Tons of cleanup! Fixing up some coms API Fixing up some color code too Delay is now based on elapsed time instead of built in delay function

--- spider_85.cpp
+++ spider_85.cpp
@@ -9,20 +9,7 @@
 #include "spider.h"
 #include "coms.h"
 #include "progmem.h"
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// MAIN CONFIGURATION AND DEFAULTS
-////////////////////////////////////////////////////////////////////////////////
-#define				TOTAL		  10
-#define				BRIGHT		  0xff
-uint16_t			offset		= 0;
-volatile uint8_t	pause		= 20;
-volatile uint8_t	mode		= 3;
-volatile uint8_t	direction	= 0;
-volatile color_t	color		= color_t::blue();
+#include "elapsedMillis.h"
 
 
 
@@ -30,7 +17,7 @@ volatile color_t	color		= color_t::blue();
 ////////////////////////////////////////////////////////////////////////////////
 // LIST OF COLORS
 ////////////////////////////////////////////////////////////////////////////////
-CONST color_t color_table[] = {
+const color_t color_table[] = {
 	color_t::white(),
 	color_t::red(),
 	color_t::yellow(),
@@ -49,6 +36,21 @@ CONST color_t color_table[] = {
 	color_t::purple(),
 	color_t::black(),
 };
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MAIN CONFIGURATION AND DEFAULTS
+////////////////////////////////////////////////////////////////////////////////
+#define				TOTAL		  10
+#define				BRIGHT		  0xff
+uint16_t			offset		= 0;
+volatile uint16_t	pause		= 15;
+volatile uint8_t	mode		= 3;
+volatile uint8_t	direction	= 0;
+volatile color_t	color		= color_table[6];
+elapsedMillis		timeout		= 0;
+
 
 
 
@@ -83,7 +85,7 @@ ISR(PCINT0_vect) {
 		case 0x04:	offset		=	data.word_1;	break;
 
 		case 0x05:
-			color = color_t::progmem(&color_table[data.word_1 & 0x0F]);
+			color = color_table[data.word_1 & 0x0F];
 		break;
 
 		case 0x06:
@@ -102,6 +104,7 @@ void setup_avr() {
 	if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 	strip.clear();
 	spi.enable();
+	timeout = 0;
 }
 
 
@@ -184,81 +187,51 @@ void rgbwcmyk() {
 // ??
 ////////////////////////////////////////////////////////////////////////////////
 void chase() {
-	color_t cx = (color_t) color;
-
-	uint16_t total = strip.total() << 2;
-	color_t cl;
+	uint16_t	total = strip.total() << 2;
+	color_t		cx;
 
 	for(uint16_t i=0; i<total; i+=4) {
-		cl = (color_t) (uint32_t) color;
+		cx = (color_t) color;
+
 		switch ((i+offset) & 0x1f) {
 			case 0x00:
 			case 0x0c:
-				strip.pixel(cl.right(6));
+				strip.pixel(cx.right(6));
 			break;
 
 			case 0x01:
 			case 0x0b:
-				strip.pixel(cl.right(5));
+				strip.pixel(cx.right(5));
 			break;
 
 			case 0x02:
 			case 0x0a:
-				strip.pixel(cl.right(4));
+				strip.pixel(cx.right(4));
 			break;
 
 			case 0x03:
 			case 0x09:
-				strip.pixel(cl.right(3));
+				strip.pixel(cx.right(3));
 			break;
 
 			case 0x04:
 			case 0x08:
-				strip.pixel(cl.right(2));
+				strip.pixel(cx.right(2));
 			break;
 
 			case 0x05:
 			case 0x07:
-				strip.pixel(cl.right(1));
+				strip.pixel(cx.right(1));
 			break;
 
 			case 0x06:
-				strip.pixel(cl);
+				strip.pixel(cx);
 			break;
 
 			default:
 				strip.pixel(color_t::black());
 		}
 	}
-
-
-/*
-
-	uint16_t segments = (256*3) / strip.total();
-	for(uint16_t i=0; i<strip.total(); i++) {
-		strip.pixel(
-			color_t::hue((i * segments) + offset)
-		);
-	}
-
-	static color_t hue(uint16_t hue) {
-		uint8_t step = hue & 0xff;
-
-		switch ((hue >> 8) & 0x03) {
-			case 0x01:	return color_t(    0, ~step,  step);
-			case 0x02:	return color_t( step,     0, ~step);
-			default:	return color_t(~step,  step,     0);
-		}
-	}
-/ *
-	for (uint16_t i=0; i<strip.total(); i++) {
-		if (((i+offset) & 0x03) == 0x00) {
-			strip.pixel(color);
-		} else {
-			strip.pixel(color_t::black());
-		}
-	}
-*/
 }
 
 
@@ -289,7 +262,9 @@ void loop_avr() {
 	}
 
 	strip.end();
-	delay(pause);
+
+	while (timeout < pause) {}
+	timeout -= pause;
 }
 
 
